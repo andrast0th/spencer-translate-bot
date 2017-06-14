065f6748d483e01f06a6ef2e05f3cc547e0af91a
Andras Toth <atoth@hpe.com>
2017-06-14 11:29:22 +0300

    Added debug logging

--- src/co/atoth/spencertranslatebot/SpencerTranslateBot.java
+++ src/co/atoth/spencertranslatebot/SpencerTranslateBot.java
@@ -3,15 +3,15 @@ package co.atoth.spencertranslatebot;
 import com.ullink.slack.simpleslackapi.SlackChannel;
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.SlackUser;
-import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
-import com.ullink.slack.simpleslackapi.events.SlackMessageUpdated;
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
-import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
-import com.ullink.slack.simpleslackapi.listeners.SlackMessageUpdatedListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class SpencerTranslateBot {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpencerTranslateBot.class);
 
     public static final String SLACK_API_KEY = System.getProperty("slackApiKey");
     public static final String GOOGLE_API_KEY = System.getProperty("googleApiKey");
@@ -19,28 +19,24 @@ public class SpencerTranslateBot {
 
     public static void main(String[] args) throws IOException {
 
+        logger.debug("SLACK_API_KEY: " + SLACK_API_KEY);
+        logger.debug("GOOGLE_API_KEY: " + GOOGLE_API_KEY);
+        logger.debug("CHANNEL_NAME: " + CHANNEL_NAME);
+
         SlackSession session = SlackSessionFactory.createWebSocketSlackSession(SLACK_API_KEY);
-        session.addMessagePostedListener(new SlackMessagePostedListener() {
-            @Override
-            public void onEvent(SlackMessagePosted event, SlackSession session) {
+        session.addMessagePostedListener((event, session12) -> {
+            SlackChannel channel = event.getChannel();
+            SlackUser sender = event.getSender();
+            String message  = event.getMessageContent();
 
-                SlackChannel channel = event.getChannel();
-                SlackUser sender = event.getSender();
-                String message  = event.getMessageContent();
-
-                TranslateMessageHandler.onEvent(message, channel, sender, session);
-            }
+            TranslateMessageHandler.onEvent(message, channel, sender, session12);
         });
-        session.addMessageUpdatedListener(new SlackMessageUpdatedListener() {
-            @Override
-            public void onEvent(SlackMessageUpdated event, SlackSession session) {
+        session.addMessageUpdatedListener((event, session1) -> {
+            SlackChannel channel = event.getChannel();
+            SlackUser sender = null;
+            String message  = event.getNewMessage();
 
-                SlackChannel channel = event.getChannel();
-                SlackUser sender = null;
-                String message  = event.getNewMessage();
-
-                TranslateMessageHandler.onEvent(message, channel, sender, session);
-            }
+            TranslateMessageHandler.onEvent(message, channel, sender, session1);
         });
         session.connect();
     }
