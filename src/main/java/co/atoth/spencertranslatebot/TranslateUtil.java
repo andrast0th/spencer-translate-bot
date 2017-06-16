45c8d506a2f9283d17949259481130871008be9c
Andras Toth <atoth@hpe.com>
2017-06-16 17:52:48 +0300

    Translation will now add the detected certainty to the message

--- src/main/java/co/atoth/spencertranslatebot/TranslateUtil.java
+++ src/main/java/co/atoth/spencertranslatebot/TranslateUtil.java
@@ -9,11 +9,16 @@ import com.google.common.collect.ImmutableList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class TranslateUtil {
 
+    private static final DecimalFormat df = new DecimalFormat("#");
     private static final Logger logger = LoggerFactory.getLogger(TranslateUtil.class);
+
+    public static final String[] translatedLanguages = new String[]{"ro", "iw"};
+    public static final String TRANSLATE_TO_LANG = "en";
 
     public static String translateIfNeeded(String message, int minimumConfidence){
 
@@ -25,18 +30,23 @@ public class TranslateUtil {
         logger.debug("Detecting lang for message: " + message);
         for (Detection detection : detections) {
             String lang = detection.getLanguage();
-            float conf = detection.getConfidence();
+            float conf = detection.getConfidence() * 100;
+            String confStr = df.format(conf) + "%";
 
             logger.debug("Detected lang " + lang + " with confidence " + conf);
-
-            if("ro".equals(lang) && conf >= (minimumConfidence / 100f)){
-                return translateText(message,"ro", "en");
-            } else if ("iw".equals(lang) && conf >= (minimumConfidence / 100f)){
-                return translateText(message,"iw", "en");
+            for(String supportedLang : translatedLanguages){
+                if(supportedLang.equals(lang) && conf >= minimumConfidence) {
+                    String translation = translateText(message, supportedLang, TRANSLATE_TO_LANG);
+                    return getFlagForLang(lang) + ": " + confStr + "\n" + translation;
+                }
             }
         }
 
-        return message;
+        return null;
+    }
+
+    public static String getFlagForLang(String lang){
+        return ":flag-"+lang+":";
     }
 
     public static String translateText(
